46d9c43ffd7b7fa924cf6e047afc92db8e65792b
vagizmagdeev <vagiz.magdeev@sailplay.ru>
2021-03-12 14:49:06 +0400

    feat: code complete

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,114 +1,64 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+    <label for="gl">oбщая длина</label>
+    <input type="number" name="gl" v-model="generalLength">
+    <input v-model="generalLength" type="range" min="1" max="20">
+
+    <label for="gw">oбщая ширина</label>
+    <input type="number" name="gw" v-model="generalWidth">
+    <input v-model="generalWidth" type="range" min="1" max="20">
+
+    <label for="ph">высота параллелепипедов</label>
+    <input type="number" name="ph" v-model="parallelepipedHeight">
+    <input v-model="parallelepipedHeight" type="range" min="1" max="4">
+
+    <label for="pd">габарит параллелепипедов</label>
+    <input type="number" name="pd" v-model="parallelepipedDimension">
+    <input v-model="parallelepipedDimension" type="range" min="1" max="4">
+
+    <vgl-renderer class="getting-started" camera="camera" scene="scene">
+      <vgl-box-geometry name="box" :width="parallelepipedDimension" :height="1" :depth="parallelepipedDimension"></vgl-box-geometry>
+      <vgl-scene name="scene">
+        <vgl-mesh geometry="box" :position="`-${generalWidth/2}, 0, -${generalLength/2}`"></vgl-mesh>
+        <vgl-mesh geometry="box" :position="`${generalWidth/2}, 0, -${generalLength/2}`"></vgl-mesh>
+        <vgl-mesh geometry="box" :position="`-${generalWidth/2}, 0, ${generalLength/2}`"></vgl-mesh>
+        <vgl-mesh geometry="box" :position="`${generalWidth/2}, 0, ${generalLength/2}`"></vgl-mesh>
+      </vgl-scene>
+      <vgl-orthographic-camera name="camera" orbit-position="10 0 0" :zoom="20" />
+    </vgl-renderer>
   </div>
 </template>
 
 <script>
+import { VglRenderer, VglScene, VglOrthographicCamera, VglBoxGeometry , VglMesh } from "vue-gl";
 export default {
   name: "HelloWorld",
+  components: {
+    VglRenderer,
+    VglScene,
+    VglOrthographicCamera,
+    VglBoxGeometry,
+    VglMesh
+  },
   props: {
     msg: String
+  },
+  data() {
+    return {
+      generalLength: 20,
+      generalWidth: 20,
+      parallelepipedHeight: 1,
+      parallelepipedDimension: 1
+    }
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.getting-started {
+  height: 420px;
+  width: 420px;
 }
 </style>
